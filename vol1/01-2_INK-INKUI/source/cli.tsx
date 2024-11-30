c7b573a31f946981804d24b6690c58ecc72eaf18
kght6123 <i.feeling.song+github@gmail.com>
2024-11-30 23:03:38 +0900

    update

---
+++ vol1/01-2_INK-INKUI/source/cli.tsx
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './app.js';
+
+const cli = meow(
+	`
+	Usage
+	  $ 01-2-ink-inkui
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ 01-2-ink-inkui --name=Jane
+	  Hello, Jane
+`,
+	{
+		importMeta: import.meta,
+		flags: {
+			name: {
+				type: 'string',
+			},
+		},
+	},
+);
+
+render(<App name={cli.flags.name} />);
