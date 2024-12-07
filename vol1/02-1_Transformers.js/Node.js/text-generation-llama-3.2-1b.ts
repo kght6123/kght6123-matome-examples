0ba4abe60045858621396d17feca18bf424d1ce6
kght6123 <i.feeling.song+github@gmail.com>
2024-12-08 00:40:06 +0900

    update

---
+++ vol1/02-1_Transformers.js/Node.js/text-generation-llama-3.2-1b.ts
@@ -0,0 +1,36 @@
+import { pipeline, TextGenerationSingle } from "@huggingface/transformers";
+
+const isTextGenerationSingleArray = (output: TextGenerationSingle[] | TextGenerationSingle[][]): output is TextGenerationSingle[] => {
+  return Array.isArray(output)
+     && output.length > 0
+     && "generated_text" in output[0]
+     && (typeof output[0].generated_text === "string" || (
+      Array.isArray(output[0].generated_text)
+        && output[0].generated_text.length > 0
+        && typeof output[0].generated_text[0].content === "string"
+        && typeof output[0].generated_text[0].role === "string"
+      ))
+     ;
+};
+
+// Create a text-generation pipeline
+const generator = await pipeline(
+  "text-generation",
+  "onnx-community/Llama-3.2-1B-Instruct",
+);
+
+// Define the list of messages
+const messages = [
+  { role: "system", content: "宇宙人の真似をして回答してください。" },
+  { role: "user", content: "自己紹介をしてください。" },
+];
+
+// Generate a response
+const output: TextGenerationSingle[] | TextGenerationSingle[][] = await generator(messages, { max_new_tokens: 128 });
+
+console.log(JSON.stringify(output));
+if (isTextGenerationSingleArray(output)) {
+  console.log(output[0].generated_text);
+} else {
+  console.log(output[0][0].generated_text);
+}
